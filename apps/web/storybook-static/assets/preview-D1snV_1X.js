314a4607476a70ef101d7fa672ef19b8a91e3adb
Nick Villapiano <nickv@illapiano.com>
2026-05-27 21:38:27 -0400

    build: rebuild Storybook with updated button colors

---
+++ apps/web/storybook-static/assets/preview-D1snV_1X.js
@@ -0,0 +1,6 @@
+const n={decorators:[t=>{const e=document.getElementById("font-preloader")||document.createElement("div");return e.id="font-preloader",e.style.cssText="position:absolute;opacity:0;pointer-events:none;font-family:Inter",e.innerHTML=`
+      <span style="font-weight:400">.</span>
+      <span style="font-weight:500">.</span>
+      <span style="font-weight:600">.</span>
+      <span style="font-weight:700">.</span>
+    `,document.body.appendChild(e),t()}],parameters:{docs:{toc:!0},a11y:{config:{rules:[{id:"color-contrast",enabled:!0}]}},controls:{matchers:{color:/(background|color)$/i,date:/date$/i},expanded:!0}}};export{n as default};
